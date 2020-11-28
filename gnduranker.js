620ee0e89443a73c389e429405ffd765485bec3a
dell india <whoisBugsbunny>
2020-11-28 12:04:49 +0530

    commit 0.7.1

--- gnduranker.js
+++ gnduranker.js
@@ -1,15 +1,15 @@
-var count = 0;
+// var count = 0;
 
-function changecolor() {
-    var bg = document.getElementById("banner");
-    var bt = document.getElementById("bannertext");
-    if (count % 2 == 0) {
-        bg.classList.replace("bg-banner-p", "bg-banner-b")
-        bt.classList.replace("banner-m-text-p", "banner-m-text-b");
-    } else {
-        bg.classList.replace("bg-banner-b", "bg-banner-p")
-        bt.classList.replace("banner-m-text-b", "banner-m-text-p");
-    }
-    count += 1;
-}
-setInterval(changecolor, 1000)
+// function changecolor() {
+//     var bg = document.getElementById("banner");
+//     var bt = document.getElementById("bannertext");
+//     if (count % 2 == 0) {
+//         bg.classList.replace("bg-banner-p", "bg-banner-b")
+//         bt.classList.replace("banner-m-text-p", "banner-m-text-b");
+//     } else {
+//         bg.classList.replace("bg-banner-b", "bg-banner-p")
+//         bt.classList.replace("banner-m-text-b", "banner-m-text-p");
+//     }
+//     count += 1;
+// }
+// setInterval(changecolor, 1000)
