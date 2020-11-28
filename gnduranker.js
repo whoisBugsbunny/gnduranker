6cd336ba72ff44f5486508a5075b26525b692164
dell india <whoisBugsbunny>
2020-11-28 11:37:30 +0530

    commit 07

--- gnduranker.js
+++ gnduranker.js
@@ -1,1 +1,15 @@
-var a = 'check online github'
+var count = 0;
+
+function changecolor() {
+    var bg = document.getElementById("banner");
+    var bt = document.getElementById("bannertext");
+    if (count % 2 == 0) {
+        bg.classList.replace("bg-banner-p", "bg-banner-b")
+        bt.classList.replace("banner-m-text-p", "banner-m-text-b");
+    } else {
+        bg.classList.replace("bg-banner-b", "bg-banner-p")
+        bt.classList.replace("banner-m-text-b", "banner-m-text-p");
+    }
+    count += 1;
+}
+setInterval(changecolor, 1000)
