5089500cc4df78370ae2b948822148c87aa4d966
dell india <whoisBugsbunny>
2020-12-05 19:59:08 +0530

    commit 0.2.29

--- index.js
+++ index.js
@@ -103,8 +103,6 @@ function googleSignup() {
         var token = result.credential.accessToken;
         // The signed-in user info.
         var user = result.user;
-        console.log("token: " + token);
-        console.log("user: " + user);
         // ...
     }).catch(function(error) {
         // Handle Errors here.
@@ -114,7 +112,26 @@ function googleSignup() {
         var email = error.email;
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
-        console.log("error: " + errorMessage);
+        // ...
+    });
+}
+
+function facebookSignup() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
         // ...
     });
 }
